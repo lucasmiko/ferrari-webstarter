47cdff57cba3751666c68faa5626b4e78d3f62ce
Lucas Mikó <lucasmiko@live.com>
2021-01-19 08:46:09 -0300

    Aprendendo testes com JavaScript

---
+++ utils.spec.js
@@ -0,0 +1,19 @@
+const { appendTemplate, getQueryString } = require("./utils");
+
+it("Should create and add and element", () => {
+  const mainElement = document.createElement("main");
+
+  expect(appendTemplate(mainElement, "h1", "Hcode Lab Ferrari").outerHTML).toBe(
+    "<h1>Hcode Lab Ferrari</h1>"
+  );
+});
+
+it("Should return the data from a url string", () => {
+  const result = getQueryString(
+    "https://hcode.com.br/?page=login&form=register"
+  );
+
+  expect(JSON.stringify(result).replace("\\", "")).toEqual(
+    '{"page":"login","form":"register"}'
+  );
+});
